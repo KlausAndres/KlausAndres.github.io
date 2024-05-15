4f731b1e9e7b504b9060f73b3069f5654ae27c94
Klaus <28318034+KlausAnd@users.noreply.github.com>
2024-05-15 16:08:09 +0200

    Commit

--- python/PlotlyConfig.py
+++
@@ -1,4 +0,0 @@
-# Defines the display parameters for plotly
-
-config = dict(displaylogo=False, scrollZoom=True, modeBarButtonsToRemove=['zoomIn', 'zoomOut', 'resetScale'], modeBarButtonsToAdd=['drawline', 'drawopenpath', 'drawcircle', 'eraseshape'])
-
